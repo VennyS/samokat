dce53451612d2abd8ce3ec6c9ae102877482e2f7
VennyS <panarin.danil.08@gmail.com>
2025-08-02 13:06:37 +0500

    feat: created setting package to work with entire application

--- backend/internal/setting/setting.go
+++ backend/internal/setting/setting.go
@@ -2,11 +2,17 @@ package setting
 
 import (
 	"fmt"
+	"net/http"
+	"samokat/internal/api/categories"
 	"samokat/internal/lib/env"
 	"samokat/internal/lib/logger"
 	cfg "samokat/internal/setting/config"
+	"samokat/internal/storage"
+	"samokat/internal/storage/repository"
 	"time"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/jmoiron/sqlx"
 	"go.uber.org/zap"
 )
 
@@ -15,7 +21,7 @@ type App struct {
 	logger *zap.SugaredLogger
 }
 
-func (a *App) Loadcfg() {
+func (a *App) LoadConfig() {
 	a.cfg = cfg.Config{
 		AppEnv: env.GetString("APP_ENV", "development"),
 		Server: cfg.ServerConfig{
@@ -46,4 +52,53 @@ func (a *App) SetupLogger() {
 	if err != nil {
 		panic(fmt.Sprintf("Failed to initialize logger: %v", err))
 	}
+}
+
+func (a App) Migrate() error {
+	a.LoadConfig()
+
+	dsn := a.cfg.DB.DSN()
+
+	err := storage.Migrate(dsn)
+	return err
+}
+
+func (a App) BootstrapDB() (*sqlx.DB, error) {
+	a.LoadConfig()
+
+	dsn := a.cfg.DB.DSN()
+	db, err := storage.BootstrapDB(dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to bootstrap database: %w", err)
+	}
+
+	return db, nil
+}
+
+func (a App) MountRouter(db *sqlx.DB) *chi.Mux {
+	router := chi.NewRouter()
+
+	catCont := categories.NewController(a.logger, categories.NewService(a.logger, repository.NewCategoryRepo(a.logger, db)))
+	catCont.RegisterRoutes(router)
+
+	return router
+}
+
+func (a App) StartServer(router *chi.Mux) {
+	a.logger.Infof("Starting server on %s", a.cfg.Server.Addr)
+	if err := http.ListenAndServe(a.cfg.Server.Addr, router); err != nil {
+		a.logger.Fatalf("Failed to start server: %v", err)
+	}
+}
+
+func (a App) InitApp() {
+	a.LoadConfig()
+	a.SetupLogger()
+	db, err := a.BootstrapDB()
+	if err != nil {
+		a.logger.Fatalf("failed to bootstrap database: %v", err)
+	}
+	router := a.MountRouter(db)
+
+	a.StartServer(router)
 }
